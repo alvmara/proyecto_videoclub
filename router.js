96264f96990a2629f1b0a7db56b5cb22fb9b924b
alvaro marzo <alvaromarzo104@gmail.com>
2022-02-23 09:48:50 +0100

    creando carpeta views

--- router.js
+++ router.js
@@ -3,3 +3,9 @@ const router = require("express").Router();
 const UsuarioRouter = require("./views/UsuarioRouter");
 const PeliculasRouter = require("./views/PeliculasRouter");
 const PedidosRouter = require("./views/PedidosRouter");
+
+router.use("/usuarios", UsuarioRouter);
+router.use("/peliculas", PeliculasRouter);
+router.use("/orders", PedidosRouter);
+
+module.exports = router;
