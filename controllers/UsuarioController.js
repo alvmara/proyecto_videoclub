530231f7fd6e3dc4dce669601d3868b8e81c4897
alvaro marzo <alvaromarzo104@gmail.com>
2022-02-23 16:31:32 +0100

    creando funciones busqueda usuarios por distintos tipos y registrando usuario

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -10,4 +10,36 @@ UsuarioController.traeUsuarios = (req, res) => {
   });
 };
 
+UsuarioController.traerUsuarioId = (req, res) => {
+  //Búsqueda buscando una Id
+  Usuario.findByPk(req.params.id).then((data) => {
+    res.send(data);
+  });
+};
+
+UsuarioController.traerUsuarioEmail = (req, res) => {
+  //Búsqueda comparando un campo
+  Usuario.findOne({ where: { email: req.params.email } }).then((data) => {
+    res.send(data);
+  });
+};
+
+
+UsuarioController.registraUsuario = async (req, res) => {
+    
+    //Registrando un usuario
+    
+        let nombre = req.body.nombre;
+        let edad = req.body.edad;
+        let apellido = req.body.apellido;
+        let apodo = req.body.apodo;
+        let email = req.body.email;
+        console.log("antes de encriptar",req.body.password);
+        let contraseña = bcrypt.hashSync(req.body.contraseña, Number.parseInt(authConfig.rounds)); 
+        
+        console.log("esta es la contraseña", contraseña);
+        //Comprobación de errores.....
+        
+        
+
 module.exports = UsuarioController;
