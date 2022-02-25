fba7f1cc82d2132cbd0cdd472af7b978e6027389
alvaro marzo <alvaromarzo104@gmail.com>
2022-02-25 15:21:55 +0100

    creando usuarios para base de datos

--- seeders/20220225120202-Pelicula.js
+++ seeders/20220225120202-Pelicula.js
@@ -1,24 +1,132 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Peliculas",
+      [
+        {
+          titulo: "Best Years of Our Lives, The",
+          sinopsis:
+            "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus.",
+          adult: false,
+          popularity: 3.4,
+          image: "http://dummyimage.com/172x100.png/ff4444/ffffff",
+          fecha: "2021-09-13 01:24:00",
+          createdAt: "2021-03-02 06:11:05",
+          updatedAt: "2021-04-06 13:12:47",
+        },
+        {
+          titulo: "Pumpkin Eater, The",
+          sinopsis:
+            "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue.",
+          adult: true,
+          popularity: 5.0,
+          image: "http://dummyimage.com/199x100.png/ff4444/ffffff",
+          fecha: "2021-08-21 19:32:09",
+          createdAt: "2021-12-18 06:58:44",
+          updatedAt: "2021-09-15 03:28:28",
+        },
+        {
+          titulo: "Serial",
+          sinopsis:
+            "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+          adult: false,
+          popularity: 10.0,
+          image: "http://dummyimage.com/232x100.png/5fa2dd/ffffff",
+          fecha: "2021-07-02 13:20:48",
+          createdAt: "2021-12-16 21:08:48",
+          updatedAt: "2021-11-23 16:45:07",
+        },
+        {
+          titulo: "Dirty Pictures",
+          sinopsis:
+            "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+          adult: true,
+          popularity: 5.4,
+          image: "http://dummyimage.com/116x100.png/ff4444/ffffff",
+          fecha: "2022-01-26 08:45:27",
+          createdAt: "2021-08-26 12:48:01",
+          updatedAt: "2022-01-26 03:41:47",
+        },
+        {
+          titulo: "Look at Me (Comme une image)",
+          sinopsis:
+            "Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis.",
+          adult: true,
+          popularity: 9.7,
+          image: "http://dummyimage.com/172x100.png/ff4444/ffffff",
+          fecha: "2022-02-10 19:39:56",
+          createdAt: "2021-04-01 04:30:49",
+          updatedAt: "2022-01-18 22:56:35",
+        },
+        {
+          titulo: "Drama/Mex",
+          sinopsis:
+            "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+          adult: true,
+          popularity: 1.5,
+          image: "http://dummyimage.com/133x100.png/dddddd/000000",
+          fecha: "2021-05-17 23:56:50",
+          createdAt: "2021-07-30 12:06:35",
+          updatedAt: "2021-05-05 08:53:27",
+        },
+        {
+          titulo: "66 Scenes From America",
+          sinopsis:
+            "Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit.",
+          adult: true,
+          popularity: 3.4,
+          image: "http://dummyimage.com/168x100.png/ff4444/ffffff",
+          fecha: "2021-07-31 05:36:16",
+          createdAt: "2021-06-02 00:03:02",
+          updatedAt: "2021-03-11 15:44:03",
+        },
+        {
+          titulo:
+            "More About the Children of Noisy Village (a.k.a. More About the Children of Bullerby Village) (Mer om oss barn i Bullerbyn)",
+          sinopsis:
+            "Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+          adult: true,
+          popularity: 8.3,
+          image: "http://dummyimage.com/115x100.png/ff4444/ffffff",
+          fecha: "2021-05-12 17:31:53",
+          createdAt: "2021-08-30 22:09:23",
+          updatedAt: "2021-11-18 07:11:48",
+        },
+        {
+          titulo: "The Kick",
+          sinopsis:
+            "Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+          adult: false,
+          popularity: 8.4,
+          image: "http://dummyimage.com/166x100.png/dddddd/000000",
+          fecha: "2022-02-13 01:55:20",
+          createdAt: "2022-01-04 21:05:58",
+          updatedAt: "2021-05-30 05:00:21",
+        },
+        {
+          titulo: "Dragon Inn (Sun lung moon hak chan)",
+          sinopsis:
+            "Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum.",
+          adult: false,
+          popularity: 7.4,
+          image: "http://dummyimage.com/169x100.png/dddddd/000000",
+          fecha: "2021-03-15 17:11:27",
+          createdAt: "2021-12-30 07:59:45",
+          updatedAt: "2021-08-29 04:41:18",
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
